10a263ff3937e3a2097eada543ac577263441edc
ntrailweb <news@ntrail.org>
2025-03-12 17:00:57 -0500

    Added Newsletters
    Added Newsletters

---
+++ news_script.js
@@ -0,0 +1,57 @@
+const baseURL = "news/"; // Replace with the actual base URL
+
+function checkFileExists(url) {
+  return new Promise((resolve) => {
+    fetch(url, { method: 'HEAD' })
+      .then((response) => {
+        if (response.ok) { // Check for successful HTTP status (200-299)
+          resolve(true);
+        } else {
+          resolve(false); // Resolve with false for non-successful status (e.g., 404)
+        }
+      })
+      .catch(() => {
+        resolve(false); // Resolve with false for network or other errors
+      });
+  });
+}
+
+
+function findLatestFile(n) {
+  
+	const today = new Date();
+	const date = new Date();
+	var week = date.getDay();
+	week = week + n * 7; // 
+	
+	const current_date = new Date();
+	current_date.setDate(date.getDate() - week);
+	const year = current_date.getFullYear().toString().slice(-2);
+	const month = String(current_date.getMonth() + 1).padStart(2, '0');
+	const day = String(current_date.getDate()).padStart(2, '0');
+	
+	return `${year}_${month}_${day}.html\n`;
+}
+function fill_list()
+{
+  
+  const linkElement = document.getElementById('current');
+  linkElement.href = baseURL + findLatestFile(0) ;
+  
+  
+  var output = "<p>";
+  for(i=1;i<25;i++)
+  {
+     console.log(i);
+	 var file_name = findLatestFile(i);
+	 console.log(baseURL + file_name);
+	 
+ 
+	 if(  checkFileExists(baseURL+file_name)  )
+	 {
+	    output = output + `<a href="` + baseURL + file_name + `">`+ file_name + `</a><br />`;
+     } 		
+  }
+  output = output + "</p>";
+  document.getElementById("list").innerHTML = output; 
+}
